8cbac5c604c7afd05ad1f8947d29ca1671275847
Alex Javier <alexavier1989@gmail.com>
2024-12-30 13:47:11 -0500

    feat: Task 1 done. Add an example about salary by year for a web developer based on seniority

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-// Prevent us from attempting to use variables
-// that are not declared
 "use strict"
+// let salaryByYear = 50000;
+
+// if (salaryByYear > 80000) {
+//     console.log("You can make more than $80,000 a year like Senior Web Developer.");
+// } else if ( 55000 < salaryByYear < 65000) {
+//     console.log("You can make between $55,000 and $65,000 a year like Mid Senior Web Developer.");
+// } else {
+//     console.log("You can make up to $55,000 a year like Junior Web Developer.");
+// }
