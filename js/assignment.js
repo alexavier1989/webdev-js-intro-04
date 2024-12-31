9cdea0b9b109de7ac0722081e78b08dacdd60e4f
Alex Javier <alexavier1989@gmail.com>
2024-12-30 20:17:51 -0500

    feat: Task 2 done. Functions to manage output message by entered age, also error messages handled

--- js/assignment.js
+++ js/assignment.js
@@ -3,15 +3,41 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
+const responseEl = document.getElementById("response");
 
-// create a mutable variable called age and do not assign it a value.
+let age;
 
+function ageDisclainer(){
+    let response = "";
+    if (age >= 21) {
+        response = "You can vote and purchase alcohol.";
+    } else if (18 <= age && age < 21) {
+        response = "You can vote, but you cannot purchase alcohol.";
+    }  else {
+        response = "You cannot vote and you cannot purchase alcohol.";
+    }
+    responseEl.innerText = response;
+}
+
+function errorDisclainer(){
+    responseEl.innerText = "Please enter a valid age.";
+    ageInputEl.value = "";
+    ageInputEl.focus();
+    return;
+}
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
-    
-    // Write your code below
 
+    if (isNaN(age) || age < 1 || age > 110) {
+        responseEl.classList.add('error-text');
+        ageInputEl.classList.add('error-input');
+        errorDisclainer();
+    } else {
+        responseEl.classList.remove('error-text');
+        ageInputEl.classList.remove('error-input');
+        ageDisclainer(age);
+    }
 }
 
 
